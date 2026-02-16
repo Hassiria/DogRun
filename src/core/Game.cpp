939b8453ab5090bc618a30a58130ce4e1175a84a
Farao <luizaserian@gmail.com>
2026-02-16 16:45:26 -0300

    feat: adicionado codigo para carregar a textura do chao

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -7,7 +7,10 @@ Game::Game(int width, int height)
 {
     renderer = new Renderer2D(screenWidth, screenHeight);
 
-    player = new Player({100.0f, 100.0f}, {50.0f, 50.0f}, *renderer);
+    textureGroundBottom = renderer->LoadTexture("textures/ground_bottom.png");
+    textureGroundTop = renderer->LoadTexture("textures/ground_top.png");
+
+    player = new Player({100.0f, 400.0f}, {50.0f, 50.0f}, *renderer);
     entities.push_back(player);
 }
 
@@ -35,6 +38,23 @@ void Game::Update(float deltaTime)
 
 void Game::Render()
 {
+
+    float tileSize = 50.0f;
+    float groundLevel = 300.0f;
+
+    for (float y = 0.0f; y < 300.0f; y += tileSize)
+    {
+        for (float x = 0.0f; x < 800.0f; x += tileSize)
+        {
+            if (y >= groundLevel - tileSize){
+                renderer->DrawSprite(textureGroundTop, x, y, tileSize, tileSize);
+            }
+            else{
+                renderer->DrawSprite(textureGroundBottom, x, y, tileSize, tileSize);
+            }
+        }
+    }
+
     for (Entity* e : entities)
         e->Render(*renderer);
 }
